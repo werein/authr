272886830be8e3977e14da850918c18fd33da2a9
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2015-02-11 02:04:01 +0100

    Append migrations

--- lib/authr/engine.rb
+++ lib/authr/engine.rb
@@ -6,5 +6,13 @@ require 'simple_token_authentication'
 module Authr
   class Engine < ::Rails::Engine
     isolate_namespace Authr
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
