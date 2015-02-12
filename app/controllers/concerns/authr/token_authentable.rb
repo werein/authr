3c469beb8f7381f21b222c22906ea2d0cf7fcd09
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2015-02-12 22:22:40 +0100

    Concern allowing user sign in via token

---
+++ app/controllers/concerns/authr/token_authentable.rb
@@ -0,0 +1,21 @@
+module Authr
+  module TokenAuthentable
+    extend ActiveSupport::Concern
+
+    included do
+      before_filter :authenticate_user_from_token!
+    end
+
+    private
+      def authenticate_user_from_token!
+        authenticate_with_http_token do |token, options|
+          user_email = options[:email].presence
+          user       = user_email && Authr::User.find_by_email(user_email)
+
+          if user && Devise.secure_compare(user.authentication_token, token)
+            sign_in user, store: false
+          end
+        end
+      end
+  end
+end
